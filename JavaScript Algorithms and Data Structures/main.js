69bab5225dc92441cd4185d83e85a49be71ac9c6
Alisher <98706585+alisher-kadraliev@users.noreply.github.com>
2022-07-24 19:17:20 +0300

    29/29 complete chalange

--- JavaScript Algorithms and Data Structures/main.js
+++ JavaScript Algorithms and Data Structures/main.js
@@ -171,3 +171,159 @@ function makeList(arr) {
 }
 
 const failuresList = makeList(result.failure);
+
+//Write Concise Object Literal Declarations Using Object Property Shorthand
+const createPerson = (name, age, gender) => {
+    // Only change code below this line
+    return {
+        name,
+        age,
+        gender
+    };
+    // Only change code above this line
+};
+
+//Write Concise Declarative Functions with ES6
+// Only change code below this line
+const bicycle = {
+    gear: 2,
+    setGear(newGear) {
+        this.gear = newGear;
+    }
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
+
+//Use class Syntax to Define a Constructor Function
+// Only change code below this line
+class Vegetable {
+    constructor(name) {
+        this.name = name;
+    }
+}
+// Only change code above this line
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // Should display 'carrot'
+
+
+//Use getters and setters to Control Access to an Object
+class Thermostat {
+    constructor(fahrenheit) {
+        this.fahrenheit = fahrenheit;
+    }
+
+    get temperature() {
+        return (5 / 9) * (this.fahrenheit - 32);
+    }
+
+    set temperature(celsius) {
+        this.fahrenheit = (celsius * 9.0) / 5 + 32;
+    }
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
+
+//Create a Module Script
+<html>
+    <body>
+        <!-- Only change code below this line -->
+        <script type="module" src="index.js"></script>
+        <!-- Only change code above this line -->
+    </body>
+</html>
+
+
+//Use export to Share a Code Block
+
+const uppercaseString = (string) => {
+    return string.toUpperCase();
+}
+
+const lowercaseString = (string) => {
+    return string.toLowerCase()
+}
+export { uppercaseString, lowercaseString }
+
+//Reuse JavaScript Code Using import
+import { uppercaseString, lowercaseString } from './string_functions.js';
+
+// Only change code above this line
+
+uppercaseString("hello");
+lowercaseString("WORLD!");
+
+//Use * to Import Everything from a File
+import * as stringFunctions from "./string_functions.js";
+
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
+
+//Create an Export Fallback with export default
+export default function subtract(x, y) {
+    return x - y;
+}
+
+import subtract from "./math_functions.js";
+
+// Only change code above this line
+
+
+//Import a Default Export
+
+subtract(7, 4);
+
+//Create a JavaScript Promise
+const makeServerRequest = new Promise((resolve, reject) => {
+
+});
+
+//Complete a Promise with resolve and reject
+const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer represents a response from a server
+    let responseFromServer;
+
+    if (responseFromServer) {
+        resolve("We got the data");
+    } else {
+        reject("Data not received");
+    }
+});
+
+//Handle a Fulfilled Promise with then
+const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer is set to true to represent a successful response from a server
+    let responseFromServer = true;
+
+    if (responseFromServer) {
+        resolve("We got the data");
+    } else {
+        reject("Data not received");
+    }
+});
+makeServerRequest.then(result => {
+    console.log(result);
+});
+
+//Handle a Rejected Promise with catch
+const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer is set to false to represent an unsuccessful response from a server
+    let responseFromServer = false;
+
+    if (responseFromServer) {
+        resolve("We got the data");
+    } else {
+        reject("Data not received");
+    }
+});
+
+makeServerRequest.catch(error => {
+    console.log(error);
+});
