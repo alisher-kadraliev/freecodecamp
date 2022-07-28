f6fd2afe841bf06cc13fd389bdcb7dfb340ef462
Alisher <98706585+alisher-kadraliev@users.noreply.github.com>
2022-07-28 19:00:51 +0300

    15/33

--- JavaScript Algorithms and Data Structures/Regular Expressions/index.js
+++ JavaScript Algorithms and Data Structures/Regular Expressions/index.js
@@ -52,3 +52,25 @@ let result = quoteSample.match(myRegex); // Change this line
 let quoteSample = "3 blind mice.";
 let myRegex = /[^aeiou^3]/gi; // Change this line
 let result = quoteSample.match(myRegex); // Change this line
+
+//Match Characters that Occur One or More Times
+let difficultSpelling = "Mississippi";
+let myRegex = /s+/g; // Change this line
+let result = difficultSpelling.match(myRegex);
+
+//Match Characters that Occur Zero or More Times
+// Only change code below this line
+let chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!"
+let chewieRegex = /Aa*/; // Change this line
+// Only change code above this line
+
+let result = chewieQuote.match(chewieRegex);
+
+//Find Characters with Lazy Matching
+let text = "<h1>Winter is coming</h1>";
+let myRegex = /<.*?>/; // Change this line
+let result = text.match(myRegex);
+
+
+//Find One or More Criminals in a Hunt
+let reCriminals = /C+/; // Change this line
