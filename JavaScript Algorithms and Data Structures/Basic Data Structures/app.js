126b92050d8614abfc26863007f1f8ea91a99547
Alisher <98706585+alisher-kadraliev@users.noreply.github.com>
2022-08-01 18:31:30 +0300

    8/20 completet

--- JavaScript Algorithms and Data Structures/Basic Data Structures/app.js
+++ JavaScript Algorithms and Data Structures/Basic Data Structures/app.js
@@ -28,3 +28,52 @@ function popShift(arr) {
 }
 
 console.log(popShift(['challenge', 'is', 'not', 'complete']));
+
+
+//Remove Items Using splice()
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+arr.splice(1, 4)
+// Only change code above this line
+console.log(arr);
+
+//Add Items Using splice()
+function htmlColorNames(arr) {
+    // Only change code below this line
+    arr.splice(0, 2, "DarkSalmon", "BlanchedAlmond")
+    // Only change code above this line
+    return arr;
+}
+
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+
+function forecast(arr) {
+    // Only change code below this line
+    return arr.slice(2, 4)
+
+}
+
+
+//Copy Array Items Using slice()
+function forecast(arr) {
+    // Only change code below this line
+    return arr.slice(2, 4)
+
+}
+
+// Only change code above this line
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+//Copy an Array with the Spread Operator
+function copyMachine(arr, num) {
+    let newArr = [];
+    while (num >= 1) {
+        // Only change code below this line
+
+        // Only change code above this line
+        num--;
+    }
+    return newArr;
+}
+
+console.log(copyMachine([true, false, true], 2));
