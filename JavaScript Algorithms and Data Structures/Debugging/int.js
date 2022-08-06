b218b46bb361eb245f578d91e93fb41f689140c1
Alisher <98706585+alisher-kadraliev@users.noreply.github.com>
2022-08-06 21:24:41 +0300

    Debugging test complited

--- JavaScript Algorithms and Data Structures/Debugging/int.js
+++ JavaScript Algorithms and Data Structures/Debugging/int.js
@@ -69,3 +69,42 @@ let base = 2;
 let exp = 3;
 let power = raiseToPower(base, exp);
 console.log(power);
+
+//Catch Off By One Errors When Using Indexing
+function countToFive() {
+    let firstFive = "12345";
+    let len = firstFive.length;
+    // Only change code below this line
+    for (let i = 0; i < len; i++) {
+        // Only change code above this line
+        console.log(firstFive[i]);
+    }
+}
+//Use Caution When Reinitializing Variables Inside a Loop
+function zeroArray(m, n) {
+    // Creates a 2-D array with m rows and n columns of zeroes
+    let newArray = [];
+    for (let i = 0; i < m; i++) {
+        let row = [];
+        // Adds the m-th row into newArray
+
+        for (let j = 0; j < n; j++) {
+            // Pushes n zeroes into the current row to create the columns
+            row.push(0);
+        }
+        // Pushes the current row, which now has n zeroes in it, to the array
+        newArray.push(row);
+    }
+    return newArray;
+}
+
+let matrix = zeroArray(3, 2);
+console.log(matrix);
+
+
+//Prevent Infinite Loops with a Valid Terminal Condition
+function myFunc() {
+    for (let i = 1; i <= 4; i += 2) {
+        console.log("Still going!");
+    }
+}
